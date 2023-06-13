bf0342ddfdd06d04eff9dfa38845eaae8540de45
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-06-13 12:26:24 +0200

    corrected exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,40 @@
 // Chiedi all'utente il numero di chilometri da percorrere
-let km = prompt("Inserisci il numero di chilometri da percorrere:");
+const km = parseInt (prompt("Inserisci il numero di chilometri da percorrere:"));
 
-// Chiedi all'utente l'età del passeggero
-let age = prompt("Inserisci l'età del passeggero:");
+console.log('km', km, typeof km)
+
+
+
 
 let pricexKm = 0.21;
 let totalPrice = km * pricexKm;
 
+if(isNaN(km)){
+  alert('Ti avevo detto di inserire un numero')
+}
+else if(km <= 0){
+  alert('Inserisci un numero maggiore di 0')
+}
+else{
 
-if (age < 18) {
-  // Sconto del 20% per i minorenni
-  totalPrice = totalPrice * 0.8;
-} else if (age > 65) {
-  // Sconto del 40% per gli over 65
-  totalPrice = totalPrice * 0.6;
+    // Chiedi all'utente l'età del passeggero
+  const age = parseInt (prompt("Inserisci l'età del passeggero:"));
+
+  console.log('age', age, typeof age)
+  if (isNaN(age)){
+    alert('Inserisci un numero valido in cifre numeriche')
+  }
+  else if (age < 18) {
+    // Sconto del 20% per i minorenni
+    totalPrice = totalPrice * 0.8;
+  } else if (age > 65) {
+    // Sconto del 40% per gli over 65
+    totalPrice = totalPrice * 0.6;
+  }
 }
 
 // Prezzo totale
-console.log("Il prezzo del biglietto è: " + totalPrice.toFixed(2) + "€");
-
+console.log("Il prezzo del biglietto è: " + totalPrice.toFixed(2) + "€", typeof totalPrice);
 
 const ticketPrice = document.getElementById('ticketPrice-container')
 
@@ -28,7 +44,6 @@ const previousText = ticketPrice.innerHTML;
 
 console.log('previousText', previousText);
 
-
 // Biglietto Finale
 ticketPrice.innerHTML = previousText + totalPrice.toFixed(2) + '€';
 
@@ -36,5 +51,10 @@ ticketPrice.innerHTML = previousText + totalPrice.toFixed(2) + '€';
 
 
 
+// discount = (0.21*km)*0.2   20%
+// discount = (0.21*km)*0.4   40%
+
+//totalCost = (0.21*km) - discount; 
+ 
 
 
